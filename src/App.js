f49724aa2a253de430b3a9dd0cdc06dfb9c88358
Daniel Avzaradel <daniela@cgen.com>
2022-06-23 00:46:36 +0300

    creating themes, added topbar and darkmode switch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import Home from './components/Home';
+import { useState } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Topbar from './components/Topbar';
+
+import { Box } from '@mui/material';
 
 function App() {
-  return <Home />;
+  const [mode, setMode] = useState('light');
+  const toggleTheme = (mode) => {
+    setMode((mode) => (mode === 'light' ? 'dark' : 'light'));
+  };
+
+  const theme = createTheme({
+    palette: {
+      mode: mode,
+      primary: {
+        main: '#B06E69',
+        light: '#E58A6A',
+        dark: '#9F4336',
+        contrastText: '#EEE9C2',
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Box bgcolor='background.default' sx={{ height: '100vh' }}>
+        <Topbar {...{ mode, toggleTheme }} />
+      </Box>
+    </ThemeProvider>
+  );
 }
 
 export default App;
