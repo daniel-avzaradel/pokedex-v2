f13d070b91e0a18b2cba8c78ee43170cefe855b3
Daniel Avzaradel <daniela@cgen.com>
2022-06-29 23:37:20 +0300

    created stats-colors file and working with stats bars

--- src/components/Pokedex/PokemonPage/PokemonStats.jsx
+++ src/components/Pokedex/PokemonPage/PokemonStats.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Box, Typography } from '@mui/material';
 
 import typeColors from '../../type-colors';
+import statsColors from './stats-colors';
 
 // styles
 import { useStyles } from './PokemonPageStyles';
@@ -9,6 +10,39 @@ import { useStyles } from './PokemonPageStyles';
 const PokemonStats = ({ pokemon }) => {
   const classes = useStyles();
   const TYPE_COLORS = typeColors;
+  const stats_colors = statsColors;
+
+  const statBarWidth = (stat) => {
+    let width = stat.base_stat;
+    if (width >= 150) {
+      return '100%';
+    } else if (width >= 120) {
+      return '85%';
+    } else if (width >= 90) {
+      return '60%';
+    } else if (width >= 60) {
+      return '25%';
+    } else {
+      return '10%';
+    }
+  };
+
+  const statBarColor = (stat, stats_colors) => {
+    let width = stat.base_stat;
+    let color = '';
+    if (width >= 150) {
+      color = 'darkturquoise';
+    } else if (width >= 120) {
+      color = 'mediumseagreen';
+    } else if (width >= 90) {
+      color = 'yellowgreen';
+    } else if (width >= 60) {
+      color = 'gold';
+    } else {
+      color = stats_colors.orangered;
+    }
+    return color;
+  };
 
   return (
     <Box width='50%'>
@@ -18,10 +52,13 @@ const PokemonStats = ({ pokemon }) => {
       <Box display='flex' flexDirection={'column'}>
         {pokemon.stats.map((stat, i) => {
           return (
-            <Box sx={{ display: 'flex' }}>
-              <Typography paragraph lineHeight={0.5} key={i} textAlign='left'>
-                {stat.stat.name[0].toUpperCase() + stat.stat.name.substring(1)}
-              </Typography>
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+              <Box>
+                <Typography paragraph lineHeight={1} key={i} textAlign='left' m='0'>
+                  {stat.stat.name[0].toUpperCase() + stat.stat.name.substring(1)}
+                </Typography>
+              </Box>
+              <Box sx={{ height: '8px', width: statBarWidth(stat), background: statBarColor(stat, stats_colors) }}></Box>
             </Box>
           );
         })}
---
+++ src/components/Pokedex/PokemonPage/stats-colors.js
@@ -0,0 +1,5 @@
+const statsColors = {
+  orangered: 'linear-gradient(138deg, rgba(36,10,0,1) 0%, rgba(255,69,0,1) 100%)',
+};
+
+export default statsColors;
